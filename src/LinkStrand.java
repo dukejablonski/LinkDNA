484dc3b9be263451ea5d7eb400b71b0775b2a4f8
Tommy Jablonski <Taj36@duke.edu>
2021-10-09 16:44:11 -0400

    joy

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -1,3 +1,44 @@
 //Joy Liu
-public class LinkStrand {
+public class LinkStrand implements IDnaStrand {
+    public LinkStrand() {
+        this("");
+    }
+    public LinkStrand(String s) {
+        initialize(s);
+    }
+    @Override
+    public long size() {
+        return 0;
+    }
+
+
+    @Override
+    public void initialize(String source) {
+
+    }
+
+    @Override
+    public IDnaStrand getInstance(String source) {
+        return null;
+    }
+
+    @Override
+    public IDnaStrand append(String dna) {
+        return null;
+    }
+
+    @Override
+    public IDnaStrand reverse() {
+        return null;
+    }
+
+    @Override
+    public int getAppendCount() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
 }
